749dcadab27687b5be6c038a6c635f3fb25d8272
Alexander Verevkin <89090823215@yandex.ru>
2025-11-25 16:44:43 +0500

    Boop

--- src/core/CAT.h
+++ src/core/CAT.h
@@ -6,21 +6,19 @@
 #include <optional>
 #include <cstdint>
 
-//#include <Windows.h>
-
 struct cut_and_transpose
 {
-	std::uint8_t min, max;
+	std::uint8_t min_val, max_val;
 	std::int16_t transpose_val;
 
-	cut_and_transpose(std::uint8_t min, std::uint8_t max, std::int16_t transpose_val = 0) noexcept:
-		min(min), max(max), transpose_val(transpose_val) {}
+	cut_and_transpose(std::uint8_t min, std::uint8_t max_val, std::int16_t transpose_val = 0) noexcept:
+		min_val(min), max_val(max_val), transpose_val(transpose_val) {}
 
 	inline std::optional<std::uint8_t> process(std::uint8_t value) noexcept
 	{
-		if (value <= max && value >= min)
+		if (value <= this->max_val && value >= this->min_val)
 		{
-			std::int16_t svalue = (std::int16_t)value + transpose_val;
+			std::int16_t svalue = (std::int16_t)value + this->transpose_val;
 			if (svalue < 0 || svalue>255)
 				return std::nullopt;
 			value = svalue;
