e9001de84376fc879fae2af4ca9914b737d9f245
Alexander Verevkin <89090823215@yandex.ru>
2026-01-02 22:38:54 +0500

    Something something, but first i have to check stuff

--- _SAFC_/SAFC_InnerModules/include_all.h
+++ _SAFC_/SAFC_InnerModules/include_all.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef SAFC_IM
-#define SAFC_IM 
+#ifndef SAFC_IM_INCLUDE_ALL
+#define SAFC_IM_INCLUDE_ALL
 
 #include "polyline_converter.h"
 #include "cut_and_transpose.h"
@@ -8,5 +8,6 @@
 #include "single_midi_processor_2.h"
 #include "midi_collection_threaded_merger.h"
 #include "fast_midi_checker.h"
+#include "simple_player.h"
 
 #endif
---
+++ _SAFC_/SAFC_InnerModules/simple_player.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef SAFC_SIMPLE_PLAYER
+#define SAFC_SIMPLE_PLAYER
+
+struct simple_player
+{
+};
+
+#endif
